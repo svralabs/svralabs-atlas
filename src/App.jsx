aac77e7836fd22b4a52af95b9000153ccee077f8
Svralabs <svralabs@gmail.com>
2026-08-20 11:22:08 +0700

    feat: 1:1 pixel-perfect Stitch React build

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import { Routes, Route } from 'react-router-dom';
-import { Home } from './pages/Home';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AtlasSpacesDesignedToOutlastTrend from './pages/AtlasSpacesDesignedToOutlastTrend';
+import AtlasPortfolioPhilosophyExtension from './pages/AtlasPortfolioPhilosophyExtension';
 
-function App() {
+export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<AtlasSpacesDesignedToOutlastTrend />} />
+<Route path='/atlas-spaces-designed-to-outlast-trend' element={<AtlasSpacesDesignedToOutlastTrend />} />
+<Route path='/atlas-portfolio-philosophy-extension' element={<AtlasPortfolioPhilosophyExtension />} />
+        <Route path="*" element={<AtlasSpacesDesignedToOutlastTrend />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
